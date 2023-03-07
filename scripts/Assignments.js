17bbebd89574aa6e4dda4d15c44099a554a3da64
Austin Davis <davis.austin1.1.c@gmail.com>
2023-03-07 11:34:39 -0500

    Populated cities for current assignments (multiple cities due to no actual assignment list)

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,9 +1,36 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getWalkerCities, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const walkerCities = getWalkerCities();
+const cities = getCities();
 
+//Function whose responsibility is to create array of assignments based on walkerId
+const filterWalkerCitiesByWalker = (singleWalker) => {
+    const assignments = []
+     
+ for (const assignment of walkerCities) {
+   if (assignment.walkerId === singleWalker.id) {
+           assignments.push(assignment)
+      }
+   }
+return assignments
+}
+
+//Function whose responsibility is to create array of assignment locations based on assignments variable
+const assignedCityNames = (assignments) => {
+    let cityNames = []
+    
+ for (const walkerCityAssignment of assignments) {
+    for (const city of cities) {
+        if (city.id === walkerCityAssignment.cityId) {
+             cityNames.push(city.name)
+             }
+         }
+     }
+        return cityNames.join(" and ")
+}
 
 // Function whose responsibility is to find the walker assigned to a pet
 const findPetWalker = (pet, allWalkers) => {
@@ -23,10 +50,12 @@ export const Assignments = () => {
 
     for (const currentPet of pets) {
         const currentPetWalker = findPetWalker(currentPet, walkers)
+        const assignments = filterWalkerCitiesByWalker(currentPetWalker)
+        const cities = assignedCityNames(assignments)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${cities}
             </li>
         `
     };
